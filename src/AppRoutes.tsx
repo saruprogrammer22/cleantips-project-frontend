41d4116965b63d90a78a613dc79afa27d6d392dc
Joseph dat Dev <josephdeleonpogi@gmail.com>
2024-07-21 16:47:38 +0800

    Page Routes

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,6 +2,11 @@ import { Route, Routes } from "react-router-dom"
 import Layout from "./layout/Layout"
 import HomePage from "./pages/HomePage"
 import OurServicesPage from "./pages/OurServicesPage"
+import GalleryPage from "./pages/GalleryPage"
+import OurBlogPage from "./pages/OurBlogPage"
+import AboutPage from "./pages/AboutPage"
+import ContactPage from "./pages/ContactPage"
+import CostCalculatorPage from "./pages/CostCalculatorPage"
 
 const AppRoutes = () => {
     return (
@@ -9,7 +14,12 @@ const AppRoutes = () => {
             <Route path="/" element={<Layout showHero><HomePage /></Layout>} />
             <Route />
 
-            <Route path="/services" element={<Layout showHero={false}><OurServicesPage /></Layout>} />
+            <Route path="/services" element={<Layout ><OurServicesPage /></Layout>} />
+            <Route path="/gallery" element={<Layout ><GalleryPage /></Layout>} />
+            <Route path="/blog" element={<Layout ><OurBlogPage /></Layout>} />
+            <Route path="/about" element={<Layout ><AboutPage /></Layout>} />
+            <Route path="/contact" element={<Layout ><ContactPage /></Layout>} />
+            <Route path="/cost-calculator" element={<Layout ><CostCalculatorPage /></Layout>} />
         </Routes>
     )
 }
